acdb2e623f5c156c279f8ecbbc8e0a817ca39bce
cipepser <respepic@gmail.com>
2017-01-31 21:58:05 +0900

    add statement to break if statement when p[i] < p[j]

--- src/sort.go
+++ src/sort.go
@@ -33,12 +33,15 @@ func (p runeSlice) Len() int {
 func (p runeSlice) Less(i, j int) bool {
 	var isLess bool
 	
-	// TODO: ここの比較を実装する
 	for k := 0; k < len(p[i]) && k < len(p[j]); k++ {
 		if p[i][k] < p[j][k] {
 			isLess = true
 			break
 		}
+		if p[i][k] > p[j][k] {
+			isLess = false
+			break
+		}		
 	}
 	return isLess
 }
@@ -49,21 +52,10 @@ func (p runeSlice) Swap(i, j int) {
 
 func main() {
 	s1 := "paraparaparadise"
-	// s2 := "paragraph"
-	// s := "se"
 	
 	X := makeCharngram([]rune(s1), 2)
-	// Y := makeCharngram([]rune(s2), 2)
 	
-	// fmt.Println(String(X))
-	// fmt.Println(String(Y))
-	
-	// fmt.Println(reflect.TypeOf(X))
-	
-	// for _, v := range X {
-	// 	fmt.Println(v)
-	// }
-	// fmt.Println(X[2][1])
+	fmt.Println(X)
 	sort.Sort(X)
 	fmt.Println(X)
 }
