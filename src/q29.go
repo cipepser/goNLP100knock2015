aaa6762480ead4845b054b9353c1deb9cd422af6
cipepser <respepic@gmail.com>
2017-03-18 16:17:06 +0900

    reformt q29.go

--- src/q29.go
+++ src/q29.go
@@ -4,15 +4,15 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"github.com/pkg/errors"
 	"io"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"regexp"
 	"strings"
-	"net/url"
-	"net/http"
-	"log"
-	"github.com/pkg/errors"
 )
 
 type Article struct {
@@ -29,22 +29,22 @@ func NewClient(urlStr string) (*Client, error) {
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to parse url: %s", urlStr)
 	}
-	
+
 	return &Client{parsedURL}, nil
 }
 
 type ResponceWiki struct {
 	Batchcomplete string `json:"batchcomplete"`
-	Query struct {
+	Query         struct {
 		Pages struct {
 			Num23473560 struct {
-				Pageid int `json:"pageid"`
-				Ns int `json:"ns"`
-				Title string `json:"title"`
+				Pageid          int    `json:"pageid"`
+				Ns              int    `json:"ns"`
+				Title           string `json:"title"`
 				Imagerepository string `json:"imagerepository"`
-				Imageinfo []struct {
-					URL string `json:"url"`
-					Descriptionurl string `json:"descriptionurl"`
+				Imageinfo       []struct {
+					URL                 string `json:"url"`
+					Descriptionurl      string `json:"descriptionurl"`
 					Descriptionshorturl string `json:"descriptionshorturl"`
 				} `json:"imageinfo"`
 			} `json:"23473560"`
@@ -78,12 +78,12 @@ func main() {
 			txt = article.Text
 		}
 	}
-	
+
 	// 基本情報だけ抜き出し
 	reg := regexp.MustCompile(`{{基礎情報 国[\s\S]*\n}}`)
 	txt = string(reg.FindAll([]byte(txt), -1)[0])
 	txt = strings.Replace(txt, "{{基礎情報 国\n", "", 1)
-	
+
 	// 前処理
 	// `\n|`でマッチさせると偶数回目がfindできないため`|`をダブらせる
 	txt = strings.Replace(txt, "\n|", "\n|\n|", -1)
@@ -97,18 +97,18 @@ func main() {
 	// Mapに格納
 	m := make(map[string]string)
 	regEmphasis := regexp.MustCompile(`'{2,5}`)
-	regInternalLink  := regexp.MustCompile(`\[\[.*?\]\]`)
+	regInternalLink := regexp.MustCompile(`\[\[.*?\]\]`)
 	regInternalLink1 := regexp.MustCompile(`(.*)\[\[(.*)#.*\]\](.*)`)
 	regInternalLink2 := regexp.MustCompile(`(.*)\[\[(.*)\|.*\]\](.*)`)
 	regExternalLink := regexp.MustCompile(`\[?http.*\]?`)
 	regCommentOut := regexp.MustCompile(`<!--.*-->`)
 	for _, v := range reg.FindAll([]byte(txt), -1) {
-		s := string(v[1: len(v) - 2])
+		s := string(v[1 : len(v)-2])
 		strs := strings.Split(s, " = ")
-		
+
 		// 強調マークアップ
 		tmp := regEmphasis.ReplaceAllString(strs[1], "")
-		
+
 		// 内部リンク
 		if regInternalLink.FindString(tmp) == "" {
 			m[strs[0]] = tmp
@@ -139,31 +139,31 @@ func main() {
 			m[strs[0]] = string(res)
 		}
 		// 外部リンク
-		m[strs[0]] = regExternalLink.ReplaceAllString(m[strs[0]], "")		
-		
+		m[strs[0]] = regExternalLink.ReplaceAllString(m[strs[0]], "")
+
 		// コメントアウト
 		m[strs[0]] = regCommentOut.ReplaceAllString(m[strs[0]], "")
 
 	}
-	
+
 	c, err := NewClient("https://en.wikipedia.org/w/api.php")
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	values := url.Values{}
 	values.Add("action", "query")
 	values.Add("prop", "imageinfo")
 	values.Add("format", "json")
 	values.Add("iiprop", "url")
-	values.Add("titles", "File:" + m["国旗画像"])
-	
+	values.Add("titles", "File:"+m["国旗画像"])
+
 	resp, err := http.Get(c.URL.String() + "?" + values.Encode())
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	
+
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
@@ -171,7 +171,7 @@ func main() {
 
 	respjson := ResponceWiki{}
 	json.Unmarshal([]byte(b), &respjson)
-	
+
 	fmt.Println(respjson.Query.Pages.Num23473560.Imageinfo[0].URL)
 
 }
