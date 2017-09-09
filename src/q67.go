db1c61357b018c64969e657ac1d803a703194487
cipepser <respepic@gmail.com>
2017-09-09 12:00:06 +0900

    add q67.go

---
+++ src/q67.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Artist struct {
+	Name string `json:"name"`
+	Tags []struct {
+		Count int    `json:"count"`
+		Value string `json:"value"`
+	} `json:"tags"`
+	Rating struct {
+		Count int `json:"count"`
+		Value int `json:"value"`
+	} `json:"rating"`
+	SortName string `json:"sort_name"`
+	Ended    bool   `json:"ended"`
+	Gid      string `json:"gid"`
+	ID       int    `json:"id"`
+	Area     string `json:"area"`
+	Aliases  []struct {
+		Name     string `json:"name"`
+		SortName string `json:"sort_name"`
+	} `json:"aliases"`
+	Begin struct {
+		Year  int `json:"year"`
+		Month int `json:"month"`
+		Date  int `json:"date"`
+	} `json:"begin"`
+	End struct {
+		Year  int `json:"year"`
+		Month int `json:"month"`
+		Date  int `json:"date"`
+	} `json:"end"`
+	Gender string `json:"gender"`
+	Type   string `json:"type"`
+}
+
+func main() {
+	if len(os.Args) != 1 {
+		errors.New("please input artist name.")
+	}
+	// connect to mongodb
+	session, err := mgo.Dial("mongodb://localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	db := session.DB("MusicBrainz")
+	col := db.C("artist")
+
+	query := col.Find(bson.M{"aliases.name": os.Args[1]})
+
+	var artists []Artist
+	query.All(&artists)
+
+	for _, a := range artists {
+		fmt.Println(a)
+	}
+
+}
